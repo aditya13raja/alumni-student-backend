b9120dddcfa9686bc21b97f63af07beb28576dd7
aditya13raja <aditya13raja@gmail.com>
2025-02-28 19:47:14 +0530

    Add jwt support and function to create jwt token from user_IDs

---
+++ configs/jwt_tokenizer.go
@@ -0,0 +1,21 @@
+package configs
+
+import (
+	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+var jwtSecret = os.Getenv("JWT_SECRET")
+
+func GenerateJWT(userID string) (string, error) {
+	claims := jwt.MapClaims{
+		"user_id": userID,
+		"exp":     time.Now().Add(time.Hour * 24 * 7).Unix(),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodES256, claims)
+
+	return token.SignedString([]byte(jwtSecret))
+}
