5f5e19e74388a0c5a0f03ae1db92e2ac0314857a
aditya13raja <aditya13raja@gmail.com>
2025-03-17 18:13:00 +0530

    SignIn or signUp send only needed info now, which are firstname, lastname and username only

--- utils/user_response.go
+++ utils/user_response.go
@@ -5,14 +5,8 @@ import "github.com/aditya13raja/alumni-student-backend/models"
 // FormatUserResponse removes sensitive data like password
 func FormatUserResponse(user models.User) map[string]interface{} {
 	return map[string]interface{}{
-		"first_name":   user.FirstName,
-		"last_name":    user.LastName,
-		"age":          user.Age,
-		"role":         user.Role,
-		"degree":       user.Degree,
-		"major":        user.Major,
-		"passing_year": user.PassingYear,
-		"username":     user.Username,
-		"email":        user.Email,
+		"first_name": user.FirstName,
+		"last_name":  user.LastName,
+		"username":   user.Username,
 	}
 }
