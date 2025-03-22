468e1b2219c82f89fb627b3cde20f317e063baca
aditya13raja <aditya13raja@gmail.com>
2025-03-22 19:02:03 +0530

    Create route for creating topics

--- controllers/topics_controller.go
+++ controllers/topics_controller.go
@@ -1,9 +1,37 @@
 package controllers
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"context"
+
+	"github.com/aditya13raja/alumni-student-backend/configs"
+	"github.com/aditya13raja/alumni-student-backend/models"
+
+	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 func CreateTopic(c *fiber.Ctx) error {
+	// Parse the request
+	var req *models.Topics
+	err := c.BodyParser(&req)
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid request body"})
+	}
+
+	// Create topic
+	topic := models.Topics{
+		ID:        primitive.NewObjectID(),
+		TopicName: req.TopicName,
+	}
+
+	//Create topic in database
+	_, err = configs.TopicsCollection.InsertOne(context.Background(), topic)
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Error creating topic"})
+
+	}
+
 	return c.Status(fiber.StatusOK).JSON(fiber.Map{
-		"status": "working",
+		"topic": topic,
 	})
 }
