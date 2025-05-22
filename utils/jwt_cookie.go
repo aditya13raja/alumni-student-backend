570d9f1f4283c76a59b1c1fe32094f5485556411
aditya13raja <aditya13raja@gmail.com>
2025-05-22 12:02:39 +0530

    Change token expiry to one day

--- utils/jwt_cookie.go
+++ utils/jwt_cookie.go
@@ -10,7 +10,7 @@ func SetAuthCookie(c *fiber.Ctx, token string) {
 	c.Cookie(&fiber.Cookie{
 		Name:     "access_token",
 		Value:    token,
-		Expires:  time.Now().Add(time.Hour * 24 * 7), // one week expiry
+		Expires:  time.Now().Add(time.Hour * 24), // one week expiry
 		HTTPOnly: true,
 		Secure:   false, //TODO: set to true at time of hosting
 		SameSite: "Strict",
